9717cafeef524dd946def42f7f4c2dc587423796
Tatsiana Shishova <sidneyandtanya@yandex.ru>
2024-03-09 16:40:23 +0300

    add

---
+++ Web/js/ClickToIncreaseImage.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const images = document.querySelectorAll(".gallery-image");
+  
+    images.forEach((image) => {
+      image.addEventListener("click", function () {
+        this.style.transform = "scale(1.2)";
+      });
+  
+      image.addEventListener("mouseleave", function () {
+        this.style.transform = "scale(1)";
+      });
+    });
+  });
+  
